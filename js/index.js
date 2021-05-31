9239ff40a4309370eb93576dda9f626573deff46
David Ubanyi <ubanyi@gmail.com>
2021-05-31 13:17:31 +0100

    properly formatted the date

--- js/index.js
+++ js/index.js
@@ -1,64 +1,76 @@
+import fetchGraphqlData from "./api";
+import htmlRenderer from "./templating";
+import { repoTemplate, avatarTemplate } from "./templates";
 
+//get template nodes
+const repositoryDiv = document.querySelector(".repository-wrapper");
+const profileDiv = document.querySelector(".profile-wrapper");
+const repoCounter = document.querySelector(".repository-counter");
 
-import fetchGraphqlData from './api'
-import htmlUpdater from './templating'
-import { repoTemplate, avatarTemplate } from './templates'
+//get static content nodes
+const usernameForm = document.getElementById("usernameForm");
+const errorDiv = document.getElementById("error");
+const mainContent = document.querySelector(".main");
 
-const repositoryDiv = document.querySelector('.repository-wrapper')
-const profileDiv = document.querySelector('.profile-wrapper')
-const repoCounter = document.querySelector('.repository-counter')
-const usernameForm = document.getElementById('usernameForm');
-const errorDiv = document.getElementById('error');
-const mainContent = document.querySelector('.main')
+//handle the username submission
 usernameForm.onsubmit = handleSubmit;
 
-
+//username input function handler
 async function handleSubmit(event) {
-    event.preventDefault();
-    const gitUsername = event.target.elements.username.value
-    if (gitUsername) {
-        loading(true)
-        //fire the fetch
-        try {
-            const userData = await fetchGraphqlData(gitUsername)
-            if (userData.errors || userData.documentation_url) {
-                throw (userData.errors ? userData.errors[0].message : userData.message)
-            }
-            //the data is fine 
-            else {
-                const user = userData.data.user
-                //simplify data from the api
-                const repos = [...user.repositories.nodes]
-                const profile = { name: user.name, bio: user.bio, username: user.login, avatarUrl: user.avatarUrl }
-                //update the repositories
-                htmlUpdater(repos, repositoryDiv, repoTemplate)
-                //update the profile
-                htmlUpdater(profile, profileDiv, avatarTemplate)
-                //update repo count
-                htmlUpdater(user.repositories.totalCount, repoCounter)
-                //show the page with the updated info
-                mainContent.removeAttribute('hidden')
-                usernameForm.classList.add('is-hidden')
-            }
-        }
-        catch (error) {
-            console.log(error)
-            errorDiv.removeAttribute('hidden')
-        } finally {
-            loading(false)
-        }
-
+  event.preventDefault();
+  //get inputed value (can handle validation in the future)
+  const gitUsername = event.target.elements.username.value;
+  //temp validation if there is an input, handle the async requests
+  if (gitUsername) {
+    //show loading animation
+    loading(true);
+    //fire the fetch
+    try {
+      const userData = await fetchGraphqlData(gitUsername);
+      //handle errors
+      if (userData.errors || userData.documentation_url) {
+        throw userData.errors ? userData.errors[0].message : userData.message;
+      }
+      //the data is fine
+      else {
+        const user = userData.data.user;
+        //simplify data from the api
+        const repos = [...user.repositories.nodes];
+        const profile = {
+          name: user.name,
+          bio: user.bio,
+          username: user.login,
+          avatarUrl: user.avatarUrl,
+        };
+        //update the repositories
+        htmlRenderer(repos, repositoryDiv, repoTemplate);
+        //update the profile
+        htmlRenderer(profile, profileDiv, avatarTemplate);
+        //update repo count
+        htmlRenderer(user.repositories.totalCount, repoCounter);
+        //show the page with the updated info
+        mainContent.removeAttribute("hidden");
+        usernameForm.classList.add("is-hidden");
+      }
+    } catch (error) {
+      //console the error
+      console.log(error);
+      //show the error notification
+      errorDiv.removeAttribute("hidden");
+    } finally {
+      //handle loading close
+      loading(false);
     }
-
+  }
 }
 
 function loading(state) {
-    //set the full page to show the loading animation
-    if (state) {
-        //show animation
-        console.log("loading on")
-    } else {
-        //stop animation
-        console.log("loading off")
-    }
+  //set the full page to show the loading animation
+  if (state) {
+    //show animation
+    console.log("loading on");
+  } else {
+    //stop animation
+    console.log("loading off");
+  }
 }
--- js/templating.js
+++ js/templating.js
@@ -1,13 +1,17 @@
-const htmlUpdater = (data, htmlEl, htmlTemplate) => {      
-    if(Array.isArray(data)){
-        for(let item of data){
-        htmlEl.appendChild(htmlTemplate(item))
-        }
-    } else if (htmlTemplate){
-        htmlEl.innerHTML = htmlTemplate(data)
-    } else {
-        htmlEl.innerHTML = data.toString()
+//simple function to handle rendering the data on the html
+const htmlRenderer = (data, htmlEl, htmlTemplate) => {
+  //check if the data type is an array  
+  if (Array.isArray(data)) {
+    for (let item of data) {
+      htmlEl.appendChild(htmlTemplate(item));
     }
-}
+  //check if there is a template to render with  
+  } else if (htmlTemplate) {
+    htmlEl.innerHTML = htmlTemplate(data);
+  //render directly if no template is available  
+  } else {
+    htmlEl.innerHTML = data.toString();
+  }
+};
 
-export default htmlUpdater
+export default htmlRenderer;
